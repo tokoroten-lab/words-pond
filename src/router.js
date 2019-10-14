dc46b584bf24436adae770d6b8ded1807aad79a3
tokoroten-lab <tokoroten.lab@gmail.com>
2019-10-14 23:55:57 +0900

    add drop-words view

--- src/router.js
+++ src/router.js
@@ -20,6 +20,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('./views/About.vue')
+    },
+    {
+      path: '/drop-words',
+      name: 'drop-words',
+      component: () => import('./views/DropWords.vue')
     }
   ]
 })
