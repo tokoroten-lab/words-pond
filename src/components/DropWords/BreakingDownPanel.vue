ebd8a39328bfe3ccffa478cbf2ac01657a67a2df
tokoroten-lab <tokoroten.lab@gmail.com>
2019-10-18 11:56:31 +0900

    change text breaking down system

---
+++ src/components/DropWords/BreakingDownPanel.vue
@@ -0,0 +1,41 @@
+<template>
+  <mdb-card class="mb-4">
+    <mdb-card-body>
+      <mdb-row>
+        <mdb-btn color="primary">{{ sentence.normal }}</mdb-btn>
+      </mdb-row>
+      <mdb-row>
+        <mdb-btn v-for="(word, index) in words" :key="index">
+          {{ word.normal }}
+        </mdb-btn>
+      </mdb-row>
+    </mdb-card-body>
+  </mdb-card>
+</template>
+
+<script>
+import { mdbRow, mdbCard, mdbCardBody, mdbBtn } from 'mdbvue'
+
+export default {
+  name: 'drop-words-breaking-down-panel',
+  components: {
+    mdbRow,
+    mdbCard,
+    mdbCardBody,
+    mdbBtn,
+  },
+  props: ['sentence', 'words'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.btn {
+  text-transform: none;
+}
+</style>
--- src/views/DropWords.vue
+++ src/views/DropWords.vue
@@ -6,27 +6,15 @@
           <mdb-input v-model="inputText" type="textarea" label="Input text" outline :rows="3" />
         </mdb-col>
       </mdb-row>
-      <mdb-row>
-        <mdb-col col="12">
-          <mdb-btn v-for="(sentence, idx) in sentencesList" :key="idx" :color="sentence.isCompleted ? 'primary' : ''">
-          {{ sentence.spelling }}
-          </mdb-btn>
-        </mdb-col>
-      </mdb-row>
-      <mdb-row>
-        <mdb-col col="12">
-          <mdb-btn v-for="(word, idx) in wordsList" :key="idx" :color="word.isCompleted ? 'secondary' : ''">
-            {{ word.spelling }}
-          </mdb-btn>
-        </mdb-col>
-      </mdb-row>
+      <BreakingDownPanel v-for="(sentence, index) in brokenDownText.sentences" :key="index" :sentence="sentence" :words="sentence.words" />
     </mdb-container>
-    </div>
   </div>
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbBtn } from 'mdbvue';
+import { mdbContainer, mdbRow, mdbCol, mdbInput } from 'mdbvue';
+import BreakingDownPanel from '@/components/DropWords/BreakingDownPanel.vue';
+import nlp from 'compromise'
 
 export default {
   name: 'drop-words',
@@ -35,90 +23,39 @@ export default {
     mdbRow,
     mdbCol,
     mdbInput,
-    mdbBtn,
+    BreakingDownPanel,
   },
-  data: function() {
+  data() {
     return {
       inputText: '',
     }
   },
   methods: {
-    normalizeText: function(text) {
-      let normalizedText = text
-      let replaceRulesList = [
-        {
-          pattern: /\r|\n|\r\n/g,
-          replaced: '<WordsPondNewLine>'
-        },
-        {
-          pattern: /^\s+|\s+$/g,
-          replaced: ''
-        },
-        {
-          pattern: /\s+/g,
-          replaced: ' '
-        },
-        {
-          pattern: /<WordsPondNewLine>/g,
-          replaced: '\n'
-        }
-      ]
-
-      replaceRulesList.forEach(rule => {
-        normalizedText = normalizedText.replace(rule.pattern, rule.replaced)
-      })
-
-      return normalizedText
-    },
   },
   computed: {
-    sentencesList: function() {
-      let normalizedText = this.normalizeText(this.inputText)
-      let sentence = ''
-      let sentencesList = []
-      let sentenceEndCharsList = ['.', '!', '?', '\n']
-
-      for (let i = 0; i < normalizedText.length; ++i) {
-        let currentChar = normalizedText[i]
-        sentence += currentChar
-        if (sentenceEndCharsList.includes(currentChar)) {
-          if (!sentenceEndCharsList.includes(sentence) && /[a-zA-z]+/.test(sentence)) {
-            sentence = sentence.trim()
-            if (!sentencesList.includes(sentence)) {
-              sentencesList.push({ spelling: sentence, isCompleted: true })
+    brokenDownText() {
+      let brokenDownText = {
+        original: this.inputText,
+        sentences: []
+      }
+  
+      let sentences = nlp(this.inputText).sentences().data()
+      brokenDownText.sentences = sentences.map(sentence => {
+        let words = nlp(sentence.text).terms().data()
+        return {
+          original: sentence.text,
+          normal: sentence.normal,
+          words: words.map(word => {
+            return {
+              original: word.text,
+              normal: word.normal
             }
-          }
-          sentence = ''
+          }),
         }
-      }
-
-      if (sentence.length > 0) {
-        sentencesList.push({ spelling: sentence, isCompleted: false })
-      }
-
-      return sentencesList
-    },
-    wordsList: function() {
-      let wordsList = []
-
-      this.sentencesList.forEach(sentence => {
-        let words = sentence.spelling.replace(/[,\(\)\.!?]/g, ' ').split(' ').filter(word => /^[a-zA-z]+$/.test(word))
-        words.forEach((word, idx) => {
-          wordsList.push({
-            spelling: word,
-            isCompleted: sentence.isCompleted || idx !== words.length - 1
-          })
-        })
       })
 
-      return wordsList 
-    }
+      return brokenDownText
+    },
   },
 }
 </script>
-
-<style scoped>
-.btn {
-  text-transform: none;
-}
-</style>
