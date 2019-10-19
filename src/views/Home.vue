c3725e0e2c77530f63ce0405a8facae842cbbcd4
tokoroten-lab <tokoroten.lab@gmail.com>
2019-10-19 10:51:25 +0900

    change logo from png to svg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Words Pond logo" src="../assets/words-pond-logo.png" width="256" height="256">
+    <img alt="Words Pond logo" src="../assets/words-pond-logo.svg" width="256">
     <h1>Welcome to Words Pond</h1>
     <p>Words Pond is a web application that helps you learn english words(<router-link to="/about">Click here for details</router-link>)</p>
   </div>
