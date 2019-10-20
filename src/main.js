6d7d9ef934dab8f3e807af08706a803a001f12c6
tokoroten-lab <tokoroten.lab@gmail.com>
2019-10-20 15:54:28 +0900

    install vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import 'mdbvue/build/css/mdb.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
