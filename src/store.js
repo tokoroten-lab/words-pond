e89678d5ee7d0510da2415ab8bbde456d938178a
tokoroten-lab <tokoroten.lab@gmail.com>
2019-10-22 01:30:00 +0900

    add methods that use web api with auth

--- src/store.js
+++ src/store.js
@@ -1,16 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Amplify, { API, Auth } from 'aws-amplify'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: null
+  },
+  getters: {
+    apiName() {
+      return Amplify.configure().API.endpoints[0].name
+    }
   },
   mutations: {
 
   },
   actions: {
-
+    async apiGetRequest({}, payload) {
+      const myInit = {
+        headers: {
+          Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
+        },
+        queryStringParameters: payload.params
+      }
+      return await API.get(this.getters.apiName, payload.path, myInit);
+    },
+    async apiPostRequest({}, payload) {
+      const myInit = {
+        headers: {
+          Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
+        },
+        body: payload.params
+      }
+      return await API.post(this.getters.apiName, payload.path, myInit)
+    }
   }
 })
